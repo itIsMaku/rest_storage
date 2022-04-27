6617d06ceaa0b2f275f8d2d0b323d0277aa227de
Adam Volkman <itismaku@gmail.com>
2022-04-27 14:38:17 +0100

    feat(project): initialize

---
+++ src/main/java/cz/maku/rest_storage/RestStorageApplication.java
@@ -0,0 +1,13 @@
+package cz.maku.rest_storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/maku/rest_storage/RestStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.maku.rest_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
