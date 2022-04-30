3b42319f0ded7e94107f4a0cc40caf96adc7c616
Adam Volkman <itismaku@gmail.com>
2022-04-30 21:31:14 +0100

    fix(repository): finding by identifier and datakey

--- src/main/java/cz/maku/rest_storage/model/storage/StorageRepository.java
+++ src/main/java/cz/maku/rest_storage/model/storage/StorageRepository.java
@@ -2,6 +2,7 @@ package cz.maku.rest_storage.model.storage;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,11 +12,16 @@ public interface StorageRepository extends JpaRepository<Storage, Long> {
 
     List<Storage> findByIdentifier(String identifier);
 
-    Optional<Storage> findByIdentifierAndDataKey(String identifier, String dataKey);
-
     void deleteByIdentifier(String identifier);
 
     void deleteByIdentifierAndDataKey(String identifier, String dataKey);
 
     long countAllByIdentifier(String identifier);
+
+    List<Storage> findAllByIdentifierAndDataKey(String identifier, String dataKey);
+
+    @Transactional
+    default Optional<Storage> findByIdentifierAndDataKey(String identifier, String dataKey) {
+        return findAllByIdentifierAndDataKey(identifier, dataKey).stream().findFirst();
+    }
 }
